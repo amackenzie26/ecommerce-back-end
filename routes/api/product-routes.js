6115c7d808c2bb3804d0cfe433f5823fe465cce6
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-10-27 11:59:19 -0700

    product routes update

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,12 +17,33 @@ router.get('/', (req, res) => {
   .then((products)=>{
     res.json(products)
   })
+    .catch((err)=>{
+      console.log(err);
+      res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id, 
+    },
+    include: [
+      Category, 
+      {
+        model: Tag,
+        through: ProductTag,
+      }
+    ]
+  })
+  .then((products)=>{
+    res.json(products)
+  })
+    .catch((err)=>{
+      console.log(err);
+      res.status(500).json(err);
+  })
 });
 
 // create new product
@@ -100,7 +121,19 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }, 
+  })
+  .then((products)=>{
+    res.json(products)
+  })
+    .catch((err)=>{
+      console.log(err);
+      res.status(500).json(err);
+  })
 });
+
 
 module.exports = router;
